4b5c22b7b8f79d4fc056aff0033b6de69e38e267
WiselyK <wisely.koay@yahoo.com>
2024-07-05 00:36:20 +0800

    Added utils.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # Import the Flask class from the flask module
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import pickle
 
 # Create an instance of the Flask class
@@ -20,6 +20,16 @@ def predict():
     prediction = 1 if prediction == 1 else -1
     return render_template("index.html", prediction=prediction, email=email)
 
+#Create an API route
+@app.route('/api/predict', methods=['POST'])
+def predict_api():
+    data = request.get_json(force=True)  # Get data posted as a json
+    email = data['content']
+    tokenized_email = cv.transform([email]) # X 
+    prediction = clf.predict(tokenized_email)
+    # If the email is spam prediction should be 1
+    prediction = 1 if prediction == 1 else -1
+    return jsonify({'prediction': prediction, 'email': email})  # Return prediction
 
 # Run the Flask application
 if __name__ == '__main__':
